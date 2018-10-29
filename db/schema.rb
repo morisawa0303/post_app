ac0a71e4b58da74f6478fe93be21b4d9116a3173
DESKTOP-47135VN\morisawa <lukaluka739@gmail.com>
2018-10-29 09:47:09 +0900

    [fix]mysqlのutf8化

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2018_10_20_141455) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "mail"
     t.datetime "created_at", null: false
